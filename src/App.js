cc76dc2e2d4de586efe173dbdaa4709758fd09fa
00sarbin10 <sarbin@esrtech.io>
2023-01-29 23:56:26 +0545

    Revert "todo app react tailwindcss apollo client"
    This reverts commit dd7c31a885f7d9f5729efac0c9112b948d6546f5.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { Home } from "./page";
-import ErrorPage from "./page/error";
-import { CreateTodo } from "./page/Todo/Create";
-import { EditTodo } from "./page/Todo/Edit";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "create",
-    element: <CreateTodo />,
-    },
-    {
-    path: "edit/:id",
-    element: <EditTodo />,
-    },
-    
-]);
   return (
-          <RouterProvider router={router} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
